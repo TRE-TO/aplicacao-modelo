ce2272f8230e666b371871afa82f3cad46cc1a61
Michael Schuenck dos Santos <michael@tre-to.jus.br>
2018-02-02 16:01:47 -0300

    Adição do Tachyons e criação do componente Cabeçalho.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'tachyons';
+import { Cabecalho } from './componentes/Cabecalho';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div className="fl w-100 ph3">
+        
+        <Cabecalho/>
+
+        <p className="">
+          Bem-vindo.
         </p>
       </div>
     );

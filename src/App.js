0984a1e17907748e48017f12ef45e4dba5e8891d
Michael Schuenck dos Santos <michael@tre-to.jus.br>
2018-02-02 16:26:35 -0300

    O conceito de estado e sua manipulação.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import 'tachyons';
 import { Cabecalho } from './componentes/Cabecalho';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: '',
+      funcionarios: []
+    }
+
+    this.trataAlteracao = this.trataAlteracao.bind(this);
+  }
+
+  trataAlteracao(e) {
+    this.setState({nome: e.target.value});
+  }
+
+  trataEnvio(e) {
+    this.setState(estadoAnterior => ({ 
+      funcionarios: estadoAnterior.funcionarios.concat(estadoAnterior.nome)
+    }));
+  }
+  
   render() {
     return (
       <div className="fl w-100 ph3">
@@ -10,11 +31,23 @@ class App extends Component {
         <Cabecalho/>
 
         <p className="">
-          Bem-vindo.
+          Nome do Funcionário: <input type="text" value={this.state.nome} onChange={this.trataAlteracao} />
+          <button onClick={this.trataEnvio.bind(this)}>Salvar</button>
         </p>
+
+        {this.state.funcionarios.length == 0 ||
+          <div>
+            <h2>Cadastrados</h2>
+            <ol>
+              {this.state.funcionarios.map(f => <li>{f}</li>)}
+            </ol>
+          </div>
+        }
+
       </div>
     );
   }
+
 }
 
 export default App;
---
+++ src/componentes/Cabecalho.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react';
+
+export class Cabecalho extends Component {
+
+    render() {
+        return (
+            <header className="">
+            <h1 className="">Sistema Repasse</h1>
+            </header>
+        );
+    }
+}

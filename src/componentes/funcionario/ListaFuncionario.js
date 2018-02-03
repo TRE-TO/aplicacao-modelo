78fea7df6f5183fbba08c2ccae0713323371625b
Michael Schuenck dos Santos <michael@tre-to.jus.br>
2018-02-03 16:23:43 -0300

    Preparando a estrutura para funcionar com o backend.

--- src/componentes/funcionario/ListaFuncionario.js
+++ src/componentes/funcionario/ListaFuncionario.js
@@ -6,20 +6,29 @@ export default class ListaFuncionario extends Component {
         super(props);
         this.state = {
           nome: '',
-          funcionarios: []
+          email: '',
+          funcionarios: [
+              {id: 1, nome: 'José da Silva Santos', email: 'jose@tre-to.jus.br'},
+              {id: 2, nome: 'Maria de Jesus Pereira', email: 'maria@tre-to.jus.br'},
+              {id: 3, nome: 'Joaquim Teixeira Souza', email: 'joaquim@tre-to.jus.br'},
+              {id: 4, nome: 'Ana Teresa Gonçalves', email: 'ana@tre-to.jus.br'},
+              {id: 5, nome: 'Pedro Silva Soares', email: 'pedro@tre-to.jus.br'}
+          ]
         }
-    
-        this.trataAlteracao = this.trataAlteracao.bind(this);
     }
 
-    trataAlteracao(e) {
-        this.setState({nome: e.target.value});
+    trataAlteracao(nomeInput, e) {
+        this.setState({[nomeInput]: e.target.value});
     }
 
     trataEnvio(e) {
-        this.setState(estadoAnterior => ({ 
-            funcionarios: estadoAnterior.funcionarios.concat(estadoAnterior.nome)
-        }));
+        e.preventDefault();
+
+        this.setState(estadoAnterior => { 
+            funcionarios: estadoAnterior.funcionarios.concat(
+                {nome: estadoAnterior.nome, email: estadoAnterior.email}
+            )
+        });
     }
 
     render() {
@@ -28,16 +37,22 @@ export default class ListaFuncionario extends Component {
 
                 <h2>Funcionarios</h2>
 
-                <p className="">
-                    Nome do Funcionário: <input type="text" value={this.state.nome} onChange={this.trataAlteracao} />
-                    <button onClick={this.trataEnvio.bind(this)}>Salvar</button>
-                </p>
+                <form className="bg-washed-yellow pv2" onSubmit={this.trataEnvio.bind(this)}>
+                    <h3>Cadastro</h3>
+                    <div>
+                        Nome: <input type="text" value={this.state.nome} onChange={this.trataAlteracao.bind(this, 'nome')} />
+                    </div>
+                    <div>
+                        Email: <input type="text" value={this.state.email} onChange={this.trataAlteracao.bind(this, 'email')} />
+                    </div>
+                    <button type="submit">Salvar</button>
+                </form>
 
                 {this.state.funcionarios.length === 0 ||
                 <div>
                     <h2>Cadastrados</h2>
                     <ol>
-                        {this.state.funcionarios.map(f => <li>{f}</li>)}
+                        {this.state.funcionarios.map(f => <li>{f.nome}</li>)}
                     </ol>
                 </div>
                 }

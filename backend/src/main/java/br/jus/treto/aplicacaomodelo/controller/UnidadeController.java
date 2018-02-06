c855ff644e75998f4dcb439f8d1cd974dee58f21
Michael Schuenck dos Santos <michael@tre-to.jus.br>
2018-02-06 19:58:29 -0300

    Passando o URI comum para a controller, retirando das actions.

--- backend/src/main/java/br/jus/treto/aplicacaomodelo/controller/UnidadeController.java
+++ backend/src/main/java/br/jus/treto/aplicacaomodelo/controller/UnidadeController.java
@@ -4,23 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.jus.treto.aplicacaomodelo.entity.Unidade;
 import br.jus.treto.aplicacaomodelo.repository.UnidadeRepository;
 
 @RestController
+@RequestMapping("/unidades")
 public class UnidadeController {
 
 	@Autowired
 	private UnidadeRepository unidadeRepository;
 	
-	@GetMapping("/unidades")
+	@GetMapping
 	Iterable<Unidade> listarTodas() {
 		return unidadeRepository.findAll();
 	}
 	
-	@PostMapping("/unidades")
+	@PostMapping
 	void inserir(@RequestBody Unidade unidade) {
 		unidadeRepository.save(unidade);
 	}

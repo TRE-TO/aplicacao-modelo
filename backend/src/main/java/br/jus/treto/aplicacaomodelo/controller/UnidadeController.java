161fefd5138d703deaa660dc8981e9084d0dcfa6
Michael Schuenck dos Santos <michael@tre-to.jus.br>
2018-02-07 20:52:11 -0300

    Backend: Adição dos métodos para alterar, excluir e recuperar pelo id.

--- backend/src/main/java/br/jus/treto/aplicacaomodelo/controller/UnidadeController.java
+++ backend/src/main/java/br/jus/treto/aplicacaomodelo/controller/UnidadeController.java
@@ -1,10 +1,15 @@
 package br.jus.treto.aplicacaomodelo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.jus.treto.aplicacaomodelo.entity.Unidade;
@@ -30,5 +35,21 @@ public class UnidadeController {
 	@PostMapping
 	void inserir(@RequestBody Unidade unidade) {
 		unidadeRepository.save(unidade);
+	}
+
+	@PutMapping
+	@ResponseStatus(HttpStatus.OK)
+	void alterar(@RequestBody Unidade unidade) {
+		unidadeRepository.save(unidade);
+	}
+	
+	@DeleteMapping
+	void excluir(Long id) {
+		unidadeRepository.delete(id);
+	}
+
+	@GetMapping("/{id}")
+	Unidade get(@PathVariable Long id) {
+		return unidadeRepository.findOne(id);
 	}
 }

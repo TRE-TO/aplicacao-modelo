6dd3f839634e1a00571a2cd0aff4853f83f66f6a
Michael Schuenck dos Santos <michael@tre-to.jus.br>
2018-02-11 11:29:28 -0300

    Backend: permitindo acesso ao console do H2.

--- backend/src/main/java/br/jus/treto/aplicacaomodelo/config/Seguranca.java
+++ backend/src/main/java/br/jus/treto/aplicacaomodelo/config/Seguranca.java
@@ -14,6 +14,8 @@ public class Seguranca extends WebSecurityConfigurerAdapter {
 		http
 			.csrf().disable()
 	        .authorizeRequests()
-	            .antMatchers("/**").permitAll();
+	            .antMatchers("/**").permitAll()
+            .and()
+            .headers().frameOptions().disable();
 	}
 }

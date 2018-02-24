397bf1642b946b057efc65457927e6810ddf866a
Michael Schuenck dos Santos <michael@tre-to.jus.br>
2018-02-23 21:15:59 -0300

    Paginação melhor elaborada.

--- frontend/src/componentes/funcionario/ListaFuncionario.js
+++ frontend/src/componentes/funcionario/ListaFuncionario.js
@@ -12,7 +12,8 @@ export default class ListaFuncionario extends Component {
           funcionarios: [],
           fazerLogin: false,
           pagina: 0,
-          tamanho: 10
+          tamanho: 10,
+          numPaginas: 1
         }
     }
 
@@ -30,12 +31,18 @@ export default class ListaFuncionario extends Component {
                     return resp.json()
                 }
             })
-            .then(json => this.setState({funcionarios: json.content}))
+            .then(json => this.setState({funcionarios: json.content, numPaginas: json.totalPages}))
             .catch(erro => this.setState({fazerLogin: true}));
     }
 
     trataAlteracao(nomeInput, e) {
         this.setState({[nomeInput]: e.target.value});
+    }
+
+    trataPaginacao(e) {
+        this.setState({pagina: e.target.id}, () => {
+            this.trataEnvio(e);
+        });
     }
 
     trataEnvio(e) {
@@ -60,6 +67,14 @@ export default class ListaFuncionario extends Component {
     }
 
     render() {
+
+        let indicePaginas = [];
+        for (let i = 0; i < this.state.numPaginas; i++) {
+            indicePaginas.push(i);
+        }
+
+        const paginas = indicePaginas.map(i => <button id={i} key={i} onClick={this.trataPaginacao.bind(this)} >{i+1}</button>);
+
         if (this.state.fazerLogin) {
             return <Redirect to="/login" />;
         }
@@ -71,7 +86,7 @@ export default class ListaFuncionario extends Component {
                 <form className="bg-black-05 pa3" onSubmit={this.trataEnvio.bind(this)}>
                     <h3>Filtrar</h3>
                     Página:<input type="text" value={this.state.pagina} onChange={this.trataAlteracao.bind(this, 'pagina')} />
-                    Tamanho:<input type="text" value={this.state.tamanho} onChange={this.trataAlteracao.bind(this, 'tamanho')} />
+                    Qtd. Página:<input type="text" value={this.state.tamanho} onChange={this.trataAlteracao.bind(this, 'tamanho')} />
                     <div className="measure">
                         <label htmlFor="nome" className="f6 b db mb2">Nome:</label>
                         <input id="nome" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" value={this.state.nome} onChange={this.trataAlteracao.bind(this, 'nome')} />
@@ -82,6 +97,7 @@ export default class ListaFuncionario extends Component {
                 {this.state.funcionarios.length === 0 ||
                 <div>
                     <FuncionariosCadastrados funcionarios={this.state.funcionarios} />
+                    {paginas}
                 </div>
                 }
             </div>
